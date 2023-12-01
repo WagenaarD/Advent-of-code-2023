46430c608deb31b3fa8f4693910db0ffe74d53c4
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2023-12-01 11:06:08 +0100

    added one liners

--- day_01/main.py
+++ day_01/main.py
@@ -28,12 +28,6 @@ def part_one(lines: 'list[str]') -> int:
 
 @print_function()
 def part_two(lines: 'list[str]') -> int:
-    # start_nums = [re.search('\d|' + '|'.join(NUMBER_LUT), line).group() for line in lines]
-    # start_nums = [clean_number_str(num) for num in start_nums]
-    # inverse_lut = [dig[::-1] for dig in NUMBER_LUT]
-    # end_nums = [re.search('\d|' + '|'.join(inverse_lut), line[::-1]).group()[::-1] for line in lines]
-    # end_nums = [clean_number_str(num) for num in end_nums]
-    # values = [int(start + end) for start, end in zip(start_nums, end_nums)]
     num_lines = [re.findall('(?=(\d|' + '|'.join(NUMBER_LUT) + '))', line) for line in lines]
     values = [int(clean_number_str(nums[0]) + clean_number_str(nums[-1])) for nums in num_lines]
     return sum(values)
@@ -43,3 +37,7 @@ if __name__ == '__main__':
     lines = sys.stdin.read().strip().split('\n')
     part_one(lines)
     part_two(lines)
+
+    # or alternatively as one liners:
+    print('part 1:', sum([int(nums[0] + nums[-1]) for line in lines if (nums := re.findall('\d', line))]))
+    print('part 2:', sum([int((nums[0] if nums[0].isdigit() else str(NUMBER_LUT.index(nums[0]) + 1)) + (nums[-1] if nums[-1].isdigit() else str(NUMBER_LUT.index(nums[-1]) + 1))) for line in lines if (nums := re.findall('(?=(\d|' + '|'.join(NUMBER_LUT) + '))', line))]))
