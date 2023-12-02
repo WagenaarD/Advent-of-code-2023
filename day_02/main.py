a196d0b6642cead93a43ba5a1b4edd3805a93aef
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2023-12-02 10:22:13 +0100

    part_2 messy

--- day_02/main.py
+++ day_02/main.py
@@ -5,7 +5,7 @@ python3 main.py < ex
 
 Start   - 10:04:07
 Part 1  - 10:17:42
-Part 2  - 
+Part 2  - 10:22:06
 Cleanup - 
 """
 
@@ -19,6 +19,7 @@ import re
 import numpy as np
 from pprint import pprint
 from functools import cache
+import math
 
 CONFIG = {
     'red': 12,
@@ -28,27 +29,29 @@ CONFIG = {
 
 lines = sys.stdin.read().strip().split('\n')
 
-score = 0
+score_p1 = 0
+score_p2 = 0
 for idx, line in enumerate(lines):
     _, sets_str = line.split(':')
     set_lst = sets_str.split('; ')
-    legal = True
+    legal_p1 = True
+    min_p2 = {'red': 0, 'green': 0, 'blue': 0}
     for cube_set in set_lst:
-        # print(cube_set)
         for cubes in cube_set.split(', '):
-            # print(cubes)
             for color, max_items in CONFIG.items():
                 if color not in cubes:
                     continue
                 number_of_cubes = int(re.search('\d+', cubes).group())
-                # print(color, max_items, cubes, number_of_cubes, number_of_cubes > max_items)
+                min_p2[color] = max(min_p2[color], number_of_cubes)
                 if number_of_cubes > max_items:
-                    legal = False
-    if legal:
+                    legal_p1 = False
+    if legal_p1:
         print('possible', idx)
-        score += idx + 1
+        score_p1 += idx + 1
+    score_p2 += math.prod(min_p2.values())
 
-print('Part 1:', score)
+print('Part 1:', score_p1)
+print('Part 2:', score_p2)
     
         
         
