c9944045b22512a782f8539cfffd8df69d3fbd61
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2023-12-06 08:36:16 +0100

    day_06 part two

--- day_06/main.py
+++ day_06/main.py
@@ -8,6 +8,7 @@ Cleanup -
 """
 # 08:06:21
 # 08:12:40
+# 08:27:11
 
 import sys
 sys.path.insert(0, '/'.join(__file__.replace('\\', '/').split('/')[:-2]))
@@ -24,10 +25,8 @@ import math
 
 @print_function()
 def part_one(lines):
-    times = list(map(int, lines[0].split()[1:]))
-    dists = list(map(int, lines[1].split()[1:]))
-    print(times)
-    print(dists)
+    times = map(int, lines[0].split()[1:])
+    dists = map(int, lines[1].split()[1:])
     ans = 1
     for time, dist in zip(times, dists):
         solutions = 0
@@ -38,16 +37,38 @@ def part_one(lines):
             if new_dist > dist:
                 solutions += 1
         ans *= solutions
-        print(solutions)
     return ans
 
+def is_winning(button_time, record_time, record_dist):
+    speed = button_time
+    time_left = record_time - button_time
+    new_dist = speed * time_left
+    return new_dist > record_dist
 
-
-        
+@print_function()
+def part_two(lines):
+    record_time = int(''.join(lines[0].split()[1:]))
+    record_dist = int(''.join(lines[1].split()[1:]))
+    # new_dist = speed * time_left
+    #          = button_time * (record_time - button_time)
+    #          = - button_time**2 + record_time * button_time
+    # trying to find point where new_dist == record_dist
+    # record_dist = - button_time**2 + record_time * button_time
+    # button_time**2 - record_time * button_time + record_dist = 0
+    # ABC formula with
+    #  X = button_time
+    #  a = 1, b = -record_time, c = record_dist
+    #  X = (-b +/- sqrt(b**2 - 4ac)) / 2a
+    #  button_time = (record_time +/- sqrt(record_time**2 - 4*record_dist)) / 2
+    range_start = (record_time - math.sqrt(record_time**2 - 4*record_dist)) / 2
+    range_end = (record_time + math.sqrt(record_time**2 - 4*record_dist)) / 2
+    ans = math.floor(range_end) - math.ceil(range_start)
+    return ans
 
 
 if __name__ == '__main__':
     """Executed if file is executed but not if file is imported."""
     lines = sys.stdin.read().strip().split('\n')
     part_one(lines)
+    part_two(lines)
 
